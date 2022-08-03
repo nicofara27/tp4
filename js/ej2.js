eab42c4e4fe602e3b06d8df2985d90536495cd5b
NicoFara <nicolas.fara.romero@gmail.com>
2022-08-02 22:39:58 -0300

    Agregue otra solucion al ej 2 usando clases

--- js/ej2.js
+++ js/ej2.js
@@ -1,26 +1,31 @@
-// 2-Escribe un programa que cree un objeto "cuenta" con las siguientes propiedades:
-//* Una propiedad titular con el valor "Alex".
-//* Una propiedad saldo, teniendo como valor inicial 0.
-//* Un método ingresar() que permita añadir dinero a la cuenta, pasando la cantidad como parámetro
-//* Un método extraer() que permita retirar la cantidad pasada como parámetro.
-//* Un método informar() que retorne la información del estado de la cuenta. 
+// // 2-Escribe un programa que cree un objeto "cuenta" con las siguientes propiedades:
+// //* Una propiedad titular con el valor "Alex".
+// //* Una propiedad saldo, teniendo como valor inicial 0.
+// //* Un método ingresar() que permita añadir dinero a la cuenta, pasando la cantidad como parámetro
+// //* Un método extraer() que permita retirar la cantidad pasada como parámetro.
+// //* Un método informar() que retorne la información del estado de la cuenta.
 
-// Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
+// // Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
 
 let cuenta = {
-    titular: `Alex`,
-    saldo: 0,
-    ingresar(deposito){this.saldo += deposito;},
-    extraer(montoExtraer){
-        if(this.saldo >= montoExtraer){
-            this.saldo -=montoExtraer;
-        } else {
-            alert(`Usted no tiene dinero insuficiente`);
-        }
-    },
-    informar(){
-        console.log(this)
-        document.write(`<p class="texto">La cuenta perteneciente a ${this.titular} tiene un balance de ${this.saldo}<br></p>`);}
+  titular: `Alex`,
+  saldo: 0,
+  ingresar(deposito) {
+    this.saldo += deposito;
+  },
+  extraer(montoExtraer) {
+    if (this.saldo >= montoExtraer) {
+      this.saldo -= montoExtraer;
+    } else {
+      alert(`Usted no tiene dinero insuficiente`);
+    }
+  },
+  informar() {
+    console.log(this);
+    document.write(
+      `<p class="texto">La cuenta perteneciente a ${this.titular} tiene un balance de ${this.saldo}<br></p>`
+    );
+  },
 };
 
 cuenta.informar();
@@ -34,3 +39,44 @@ let montoExtraer = parseFloat(prompt(`Ingrese un monto a retirar`));
 cuenta.extraer(montoExtraer);
 
 cuenta.informar();
+
+
+//Solucion usando una clase
+
+// class Cuenta {
+//   constructor(titular, saldo) {
+//     this.titular = titular;
+//     this.saldo = saldo;
+//   }
+//   ingresar(deposito) {
+//     this.saldo += deposito;
+//   }
+//   extraer(montoExtraer) {
+//     if (this.saldo >= montoExtraer) {
+//       this.saldo -= montoExtraer;
+//     } else {
+//       alert(`Usted no tiene dinero insuficiente`);
+//     }
+//   }
+//   informar() {
+//     console.log(this);
+//     document.write(
+//       `<p class="texto">La cuenta perteneciente a ${this.titular} tiene un balance de ${this.saldo}<br></p>`
+//     );
+//   }
+// }
+
+// let cuentaAlex = new Cuenta("Alex", 100);
+
+
+// cuentaAlex.informar();
+
+// let deposito = parseFloat(prompt(`Ingrese un monto`));
+// cuentaAlex.ingresar(deposito);
+
+// cuentaAlex.informar();
+
+// let montoExtraer = parseFloat(prompt(`Ingrese un monto a retirar`));
+// cuentaAlex.extraer(montoExtraer);
+
+// cuentaAlex.informar();
